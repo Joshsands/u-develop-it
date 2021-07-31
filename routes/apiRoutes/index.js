e0a57a715ef8b35fd47aa50863ab25a623a0cdab
Joshua Sands <josh.sands@mail.com>
2021-07-31 16:09:00 -0500

    modularized apiRoutes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//Use the express.Router class to create modular, mountable route handlers.
+//A Router instance is a complete middleware and routing system; for this reason, it is often referred to as a “mini-app”.
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+module.exports = router;
